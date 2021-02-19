bdc40469a986a90f0efde3385a4e070d238d0669
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2021-02-19 22:51:58 +0100

    Update branch tp2

--- TP2/public/scripts/main.js
+++ TP2/public/scripts/main.js
@@ -3,14 +3,28 @@ import { STARSHIP_POSITION_X, STARSHIP_WIDTH } from './Source';
 import StarShip from './StarShip'
 
 const canvas = document.getElementById("stars");
+const context = canvas.getContext("2d");
 
 const starship = new StarShip(STARSHIP_POSITION_X, canvas.height / 2 - STARSHIP_WIDTH / 2, 0, 0)
 
 const theGame = new Game(canvas, canvas.width, canvas.height, starship)
 
+
+function update() {
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    theGame.initStarship()
+
+    const raf = window.requestAnimationFrame(update);
+}
+
 // mise en place de l'action des clics sur les boutons + les gestionnaires du clavier pour contrôler le starship
 const init = () => {
     theGame.initStarship()
+
+    update();
+
 }
 
 window.addEventListener("load", init);
